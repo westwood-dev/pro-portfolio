2a497d547c4bd70b9b97fcb637ff3fef5409f431
William Westwood <wwstwd@gmail.com>
2024-11-14 08:33:38 +0000

    update

---
+++ components/ProjectItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ props.project.title }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  project: {
+    title: string;
+    link: string;
+    img: string;
+  };
+}>();
+</script>
+
+<style></style>
