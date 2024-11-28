24d38a5c5147ae3015b2826925031f10f1e8d215
William Westwood <wwstwd2gmail.com>
2024-11-28 12:32:19 +0000

    update icon cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,28 @@ import { Link } from '#build/components';
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   modules: ['@nuxt/content', '@nuxt/icon', '@nuxtjs/seo'],
+
   app: {
     head: {
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }],
     },
   },
+
+  icon: {
+    clientBundle: {
+      icons: [
+        'material-symbols:dark-mode',
+        'material-symbols:light-mode',
+        'material-symbols:arrow-outward',
+        'material-symbols:arrow-forward',
+      ],
+    },
+  },
+
   ssr: true,
+
   nitro: {
     preset: 'github_pages',
   },
