caf48f7a86f690512802630aa7eda35d20bf43af
William Westwood <wwstwd@gmail.com>
2025-04-17 10:28:07 +0100

    Updated CV and theme to light

--- composables/useTheme.ts
+++ composables/useTheme.ts
@@ -2,26 +2,26 @@ import { ref, onMounted } from 'vue';
 import Theme from '../utils/theme';
 
 export const useTheme = () => {
-  const STORAGE_KEY = 'selected-theme';
-  const defaultTheme = 'red';
+        const STORAGE_KEY = 'selected-theme';
+        const defaultTheme = 'light';
 
-  const currentTheme = ref<string>(defaultTheme);
-  const theme = ref<Theme | null>(null);
+        const currentTheme = ref<string>(defaultTheme);
+        const theme = ref<Theme | null>(null);
 
-  const setTheme = (newTheme: string) => {
-    if (!theme.value || !import.meta.client) return;
-    theme.value.set(newTheme);
-    currentTheme.value = newTheme;
-  };
+        const setTheme = (newTheme: string) => {
+                if (!theme.value || !import.meta.client) return;
+                theme.value.set(newTheme);
+                currentTheme.value = newTheme;
+        };
 
-  onMounted(() => {
-    theme.value = new Theme(defaultTheme);
-    currentTheme.value = localStorage.getItem(STORAGE_KEY) || defaultTheme;
-  });
+        onMounted(() => {
+                theme.value = new Theme(defaultTheme);
+                currentTheme.value = localStorage.getItem(STORAGE_KEY) || defaultTheme;
+        });
 
-  return {
-    theme,
-    currentTheme,
-    setTheme,
-  };
+        return {
+                theme,
+                currentTheme,
+                setTheme,
+        };
 };
